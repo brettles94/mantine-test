9cc2638b1225eec04cd9ca8183772c9140719fef
Brett Orr <brett.orr@axcelerate.com>
2024-04-20 20:06:57 +1000

    Cleanup

--- out/_next/static/lPqQMeO-ViANkS1InP80I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/169-a9f5d83f3086e566.js","static/chunks/pages/index-7fc1f7879967dcb3.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
